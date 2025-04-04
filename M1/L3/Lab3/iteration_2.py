046bcdc07e21cf8bffd4dfb1f348a58579e58966
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2025-04-03 19:49:37 -0700

    Delete M1/L3/Lab3/iteration_2.py

--- M1/L3/Lab3/iteration_2.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from snowflake.cortex import Complete
-        
-st.title('❄️ LLM Demo')
-        
-prompt = st.text_input('What do you want to know?', placeholder='Ask a question')
-if st.button("Submit", type='primary'):
-    response = Complete(model='claude-3-5-sonnet', prompt=prompt)
-    st.markdown(response)
