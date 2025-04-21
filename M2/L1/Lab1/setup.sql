a84f0a4165201a22f0e591d346400911f8505a63
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2025-04-21 10:07:49 -0700

    Update setup.sql

--- M2/L1/Lab1/setup.sql
+++ M2/L1/Lab1/setup.sql
@@ -1,12 +1,19 @@
 -- Create the avalanche database and schema
-CREATE DATABASE IF NOT EXISTS avalanche;
-CREATE SCHEMA IF NOT EXISTS avalanche;
+CREATE DATABASE IF NOT EXISTS avalanche_db;
+CREATE SCHEMA IF NOT EXISTS avalanche_schema;
 
+-- Option 1: Push files to Stage from S3
 -- Create the stage for storing our files
-CREATE STAGE IF NOT EXISTS avalanche.customer_reviews
-  ENCRYPTION = (TYPE = 'SNOWFLAKE_SSE')
-  DIRECTORY = (ENABLE = true);
+CREATE OR REPLACE STAGE customer_reviews
+  URL = 's3://sfquickstarts/misc/customer_reviews/'
+  DIRECTORY = (ENABLE = TRUE AUTO_REFRESH = TRUE);
 
+-- Option 2: Manual upload to Stage
+-- Create the stage for storing our files
+--CREATE STAGE IF NOT EXISTS avalanche.customer_reviews
+  --ENCRYPTION = (TYPE = 'SNOWFLAKE_SSE')
+  --DIRECTORY = (ENABLE = true);
+--
 -- Now go and upload files to the stage. 
 -- Once you've done that proceed to the next step
   
