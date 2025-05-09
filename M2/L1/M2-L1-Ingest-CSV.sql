eaa0e33903a247441521f007c4f3d9dd361caaa3
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2025-05-08 23:53:21 -0700

    Create M2-L1-Ingest-CSV.sql

---
+++ M2/L1/M2-L1-Ingest-CSV.sql
@@ -0,0 +1,10 @@
+-- 1. Create the avalanche database and schema
+CREATE DATABASE IF NOT EXISTS avalanche_db;
+CREATE SCHEMA IF NOT EXISTS avalanche_schema;
+
+-- 2. Download CSV data 
+-- Link to download from https://github.com/https-deeplearning-ai/rapid-prototyping-with-streamlit/blob/master/M1/Lab3/data/customer_reviews.csv
+
+-- 3. Upload CSV data
+-- Data > Add Data > Load data into a Table > Browse > Select CSV file
+-- Next, in pop-up modal: Select database / Create table
